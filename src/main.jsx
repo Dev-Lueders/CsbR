57680ef3437862fcaf2cd2f3719b0889c0e5286d
Dev-Lueders <creatorsandboxreview@gmail.com>
2025-02-24 16:02:12 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import {Provider} from 'react-redux';
-import store from './redux/store';
+import store from './redux/stores/Page_Store.jsx';
 import '../src/components/components_styles.css';
 import App from './App.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
