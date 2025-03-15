ab7db81f63e7d81c6f7318c7634d1b925db1521e
Dev-Lueders <creatorsandboxreview@gmail.com>
2025-03-15 01:29:14 -0500

    #4 - Testing - 15_01-29_03-2025

--- auto_commit.ps1
+++ auto_commit.ps1
@@ -1,42 +1,50 @@
 # Auto Location
-$repoPath = Get-Location 
+	$repoPath = Get-Location 
 
 #changes directory to installed location
-cd $repoPath
+	cd $repoPath
 
 #counts the commits
-$commitFile = "commit_count.txt"
+	$commitFile = "commit_count.txt"
 
 # seeing how many commit files there are. If not start @ 1
-
-if( Test-Path $commitFile) {
-	$commitNum = [int](Get-Content $commitFile) + 1
-} else {
-	$commitNum = 1
-}
+	if( Test-Path $commitFile) {
+		$commitNum = [int](Get-Content $commitFile) + 1
+	} else {
+		$commitNum = 1
+	}
 
 # new commmit number added
-$commitNum | Out-File $commitFile
+	$commitNum | Out-File $commitFile
 
 # time stamp
-$timestamp = Get-Date -Format "dd_HH-mm_MM-yyyy"
+	$timestamp = Get-Date -Format "dd_HH-mm_MM-yyyy"
 
 
 #enter your own comment
-$customComment = Read-Host "Enter commit message"
-
+	$customComment = Read-Host "Enter commit message"
 
 
 #make commit
-$commitMessage = "Auto Commit #$commitNum - $timestamp"
+	$commitMessage = "#$commitNum - $customComment - $timestamp"
 
+#no comment, no problem
+	if ($customComment) {
+	$customMessage += " - $customComment"
+	}
+
+# Getting current branch
+	$currentBranch = git ref-parse --abbrev-rev HEAD
 
 # adding to git 
-git add .
-git commit -m $commitMessage
-git push origin main	#need to work on auto changing main
 
-Write -Output "Committed with message: $commitMessage"
+	git add .
+
+	git commit -m "$commitMessage"
+	
+	git push origin $currentBranch	
+
+	Write -Output "Committed with message: $commitMessage"
 
 
 
