7f180ff317b61a73deec9978685bc7e044011922
Dev-Lueders <creatorsandboxreview@gmail.com>
2025-03-15 05:08:33 -0500

    #7 - Testing Again - 15_05-08_03-2025 - (Committed by: 6) - Testing Again

--- auto_commit.ps1
+++ auto_commit.ps1
@@ -1,4 +1,4 @@
-# Auto Location
+# Auto Location 
 $repoPath = Get-Location 
 
 # Changes directory to the installed location
@@ -17,19 +17,50 @@ if (Test-Path $commitFile) {
 } else {
     Write-Host "Adding commit_count.txt for auto Time Stamping and Commit Count"
     Write-Host "Running auto_commit.ps1 again, moves auto_commit.ps1 to gitignore"
+    Write-Host "Will use Git Hub username or Windows Systems Name to add to comments"
 }
 
-# Counts the commits
-$commitFile = "commit_count.txt"
+# Checks for name in commit_count.txt, If not there will ask for initials
+if (Test-Path $commitFile) {
+    $name = Get-Content $commitFile
+} else {
+    $gitHubName = git config user.name
+    $windowsName = $env:USERNAME
 
-# Seeing how many commit files there are. If not, start at 1
+    Write-Host "Choose a name to store in commit_count.txt:"
+    Write-Host "1) GitHub Username: $gitHubName"
+    Write-Host "2) Windows Username: $windowsName"
+    Write-Host "3) Type in Your Initials:"
+    $choice = Read-Host "Enter 1 for GitHub or 2 for Windows username or 3 to choose your name: "
+    
+    if ($choice -eq "1" -and $gitHubName) {
+        $name = $gitHubName
+    } elseif ($choice -eq "2") {
+        $name = $windowsName
+    } elseif ($choice -eq "3") {
+        $name = Read-Host "Your initials please "
+    } else {
+        Write-Host "Invalid Choice. Defaulting to Windows Username."
+        $name = $windowsName
+    }
+
+    # Adds name to commit_count file
+    $name | Out-File -Encoding utf8 $commitFile
+}
+
+$name = Get-Content $commitFile
+
+# Checks for commit count.txt If not, start at 1
 if (Test-Path $commitFile) {
     $commitNum = [int](Get-Content $commitFile) + 1
 } else {
     $commitNum = 1
 }
 
-# New commit number added
+# Reads Username from commit_count.txt
+$name | Out-File -Encoding utf8 $commitFile
+
+# Commit number added
 $commitNum | Out-File $commitFile
 
 # Time stamp
@@ -39,11 +70,11 @@ $timestamp = Get-Date -Format "dd_HH-mm_MM-yyyy"
 $customComment = Read-Host "Enter commit message"
 
 # Make commit
-$commitMessage = "#$commitNum - $customComment - $timestamp"
+$commitMessage = "#$commitNum - $customComment - $timestamp - (Committed by: $name)"
 
 # No comment, no problem
 if ($customComment) {
-    $customMessage += " - $customComment"
+    $commitMessage += " - $customComment"
 }
 
 # Getting current branch
