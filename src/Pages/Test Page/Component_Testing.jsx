77d3f784aac4e15057d11a5805e68c9f36b35bba
Dev-Lueders <creatorsandboxreview@gmail.com>
2025-03-24 02:23:23 -0500

    #22 - Working on spacing of Main, Media, and Stats content boxes, media needs to be closer to the main and stats needs to be bigger  - 24_02-21_03-2025 - (Committed by: Dev-Lueders)

--- src/Pages/Test Page/Component_Testing.jsx
+++ src/Pages/Test Page/Component_Testing.jsx
@@ -1,14 +1,37 @@
 import React from 'react';
 import "../pages_styles.css";   
 import "../../components/components_styles.css"
-import Page_Frame from '../../components/Organisim/Page_Frame';
+import T_Navbar from '../../components/Atoms/NavBar/T_Navbar';
+import L_Navbar from '../../components/Atoms/NavBar/L_Navbar';
+import B_Navbar from '../../components/Atoms/NavBar/B_Navbar';
 import Main_Container from '../../components/Atoms/Main_Content/Main_Container';
 import Stats_Container from '../../components/Atoms/Stats_Content/Stats_Container';
 import Media_Container from '../../components/Atoms/Media_Content/Media_Container';
 import UGC_Card from '../../components/Atoms/UGC_Cards/UGC_Cards';
 const Component_Testing = () => {
+    const T_Links = [
+     { label: "Home", path: "/" },
+    ]
+    const L_Links = [
+           { path: "/Signup_Page", label: "SignUp" },
+    { path: "/Games", label: "Games"},
+    { path: "/coming_soon", label: "New Features" },
+    { path: "/Suggestion_Box", label: "Suggestion Box" },
+    ]
+    const B_Links = [
+    { path: "/SignUp_Page", label: "Signup" },
+    { path: "/Contact", label: "Contact Us" },
+    { path: "/Help", label: "Help" },
+    { path: "/About", label: "About" },
+    { path: "/Support", label: "Support" },
+    { path: "/Legal", label: "Legal" },
+    { path: "/ReadMe", label: "ReadMe Info"},]
     return (
+
         <>
+            <T_Navbar links={T_Links} />
+            <L_Navbar links={L_Links} />
+            <B_Navbar links={B_Links}/>
   <Main_Container/>
   <Stats_Container/>
   <Media_Container/>
