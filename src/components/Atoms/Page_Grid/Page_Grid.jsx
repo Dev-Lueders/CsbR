c93510b543f6b2c01991f326eb71f409a7ced478
Dev-Lueders <creatorsandboxreview@gmail.com>
2025-03-24 06:22:43 -0500

    #23 - Setup Page Grid to wrap the entire project in a Grid system - 24_06-21_03-2025 - (Committed by: Dev-Lueders)

---
+++ src/components/Atoms/Page_Grid/Page_Grid.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+const Page_Grid = ({ children, className = '', role ='main', style={}, ...props }) => {
+    return (
+        <div className={`Page-Grid ${className}`} role={role} style={style} {...props}>
+            {children}
+
+        </div>
+    );
+};
+    Page_Grid.propTypes = {
+        children: PropTypes.node.isRequired,
+        className: PropTypes.string,
+        role: PropTypes.string,
+        style: PropTypes.object,
+    
+}
+Page_Grid.defaultProps = {
+    className: '',
+    role: 'main',
+    style: {},
+
+};
+
+
+export default Page_Grid;
